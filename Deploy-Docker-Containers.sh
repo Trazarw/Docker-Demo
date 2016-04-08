aba6d65ce6269438fd3b2f55493054c754905b9c
Enis Sanchez <Enis Sanchez>
2016-04-08 02:18:43 +0100

    c

--- Deploy-Docker-Containers.sh
+++ Deploy-Docker-Containers.sh
@@ -1,17 +1,32 @@
-#Create the folder to contain the Docker build and move the terminal to the to it
-sudo mkdir "dcktmpfolder"
-cd "dcktmpfolder"
 
-#Create the server docker build and run it
-echo "yo"
-cd "$(dirname "DockerFiles/Server/'")"
-sudo docker build -t 'my-python-app' .
-sudo docker run -it --rm --name 'my-running-app' 'my-python-app'
-cd ..
 
 #Create the client docker build and run it
 #cd ".\DockerFiles\Client"
 #docker build -t "docker-client-build-image" .
 #sudo docker run "docker-client-build-image" --net bridge
 #cd "cd .."
- 
+
+#!/bin/bash
+#
+# this script should not be run directly,
+# instead you need to source it from your .bashrc,
+# by adding this line:
+#   . ~/bin/myprog.sh
+#
+
+function Deploy() {
+
+	#Create the folder to contain the Docker build and move the terminal to the to it
+  	sudo mkdir "dcktmpfolder"
+  	cd "dcktmpfolder"
+
+	#Create the server docker build and run it
+	echo "yo"
+	cd "$(dirname "DockerFiles/Server/'")"
+	sudo docker build -t 'my-python-app' .
+	sudo docker run -it --rm --name 'my-running-app' 'my-python-app'
+	cd ..
+
+}
+
+Deploy
