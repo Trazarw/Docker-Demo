cd1bf60c23e9067cfdab3b54919d7834c71af65e
Enis <enis.nefzi@veinteractive.com>
2016-04-08 14:46:24 +0100

    changes

--- Start-Script.sh
+++ Start-Script.sh
@@ -1,21 +1,31 @@
-#Update apt get
-sudo apt-get update
 
-#Install curl
-sudo apt-get install curl
 
-#Install git
-apt-get install git
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+     echo "$DIR"
+}
 
-#Install docker
-sudo curl -fsSL https://get.docker.com/ | sh
-sudo docker daemon
-sudo rm /var/run/docker.pid -f
-sudo docker daemon
+install_machine_libraries() {
+	sudo apt-get update
+	sudo apt-get install curl
+	apt-get install git
+	sudo curl -fsSL https://get.docker.com/ | sh
+	sudo docker daemon
+	sudo rm /var/run/docker.pid -f
+	sudo docker daemon
+	git clone https://github.com/Trazarw/GC-Infra
+	cd "$path/GC-Infra"
+}
 
-#Cloning the repository
-git clone https://github.com/Trazarw/GC-Infra
+deploy_docker_containers() {
+	chmod 555 Deploy-Docker-Containers
+	./Deploy-Docker-Containers "$path"
+}
 
-#Deploy the docker containers
-chmod 555 Deploy-Docker-Containers
-./Deploy-Docker-Containers
+install_machine_libraries  $(get_script_dir); deploy_docker_containers $(get_script_dir)
