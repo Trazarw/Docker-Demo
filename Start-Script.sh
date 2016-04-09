0467f62fecb06e1a1b7cb1c103b29de83f5307d8
Enis Sanchez <Enis Sanchez>
2016-04-10 00:42:50 +0100

    beta 0.1, release script

--- Start-Script.sh
+++ Start-Script.sh
@@ -1,21 +1,49 @@
 #Update apt get
-sudo apt-get update
+apt-get update
 
 #Install curl
-sudo apt-get install curl
+apt-get install curl
 
 #Install git
 apt-get install git
 
+#Purge old docker
+apt-get purge lxc-docker*
+apt-get purge docker.io*
+
+#Install CA certificates
+apt-get install apt-transport-https ca-certificates
+
+#Add the key
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+#Recreate docker file list & add a entry for Deian
+sudo rm /etc/apt/sources.list.d/docker.list
+sudo echo 'deb https://apt.dockerproject.org/repo debian-wheezy main' > /etc/apt/sources.list.d/docker.list
+
+#Verify that APT is pulling from the right repository.
+apt-cache policy docker-engine
+
 #Install docker
-sudo curl -fsSL https://get.docker.com/ | sh
-sudo docker daemon
-sudo rm /var/run/docker.pid -f
-sudo docker daemon
+sudo apt-get install docker-engine
+
+#Start docker 
+sudo service docker start
 
 #Cloning the repository
+sudo rm GC-Infra -r
 git clone https://github.com/Trazarw/GC-Infra
 
+#Get executing script source
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+	DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+	SOURCE="$( readlink "$SOURCE" )"
+	[[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+scriptPath="$( cd -P "$( dirname "$SOURCE" )" && pwd )" 
+
 #Deploy the docker containers
-chmod 555 Deploy-Docker-Containers
-./Deploy-Docker-Containers
+cd "$scriptPath/GC-Infra/"
+sudo chmod 555 "Deploy-Docker-Containers.sh"
+$scriptPath/Deploy-Docker-Containers.sh
