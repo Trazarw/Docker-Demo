8fe118cdfdb04d9afd6d67439b95ac2b1fc91e2c
Enis Sanchez <Enis Sanchez>
2016-04-10 01:03:22 +0100

    m

--- Deploy-Docker-Containers.sh
+++ Deploy-Docker-Containers.sh
@@ -1,39 +1,26 @@
 get_script_dir () {
      SOURCE="${BASH_SOURCE[0]}"
-     # While $SOURCE is a symlink, resolve it
      while [ -h "$SOURCE" ]; do
           DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
           SOURCE="$( readlink "$SOURCE" )"
-          # If $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
           [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
      done
      DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
      echo "$DIR"
 }
 
-	spath=$(get_script_dir)
-	sudo rm "dcktmpfolder" -r
-	sudo mkdir "dcktmpfolder"
-	cd "dcktmpfolder"	
-	cd "$spath/DockerFiles/Client/"
-	sudo docker build -t 'my-python-app' .
-	sudo docker run -it --rm --name 'my-running-app-01' 'my-python-app'
+spath=$(get_script_dir)
+sudo rm "dcktmpfolder" -r
+sudo mkdir "dcktmpfolder"
+cd "dcktmpfolder"	
+cd "$spath/DockerFiles/Client/"
+sudo docker build -t 'my-python-app' .
+sudo docker run -it --rm --name 'my-running-app-01' 'my-python-app'
 
-	sudo rm "dcktmpfolder" -r
-	sudo mkdir "dcktmpfolder"
-	cd "dcktmpfolder"	
-	cd "$spath/DockerFiles/Server/"
-	sudo docker build -t 'my-node-app' .
-	sudo docker run -it --rm --name 'my-running-app-02' 'my-node-app'
-#Create the folder to contain the Docker build and move the terminal to the to it
+sudo rm "dcktmpfolder" -r
+sudo mkdir "dcktmpfolder"
+cd "dcktmpfolder"	
+cd "$spath/DockerFiles/Server/"
+sudo docker build -t 'my-node-app' .
+sudo docker run -it --rm --name 'my-running-app-02' 'my-node-app'
 
-
-#Create the server docker build and run it
-
-
-#Create the client docker build and run it
-#cd ".\DockerFiles\Client"
-#docker build -t "docker-client-build-image" .
-#sudo docker run "docker-client-build-image" --net bridge
-#cd "cd .."
- 
