dd5a279a48bee7ee126f656a99289c0f0b704189
Enis Sanchez <Enis Sanchez>
2016-04-10 03:00:52 +0100

    m

--- Deploy-Docker-Containers.sh
+++ Deploy-Docker-Containers.sh
@@ -1,14 +1,14 @@
-#sudo rm "dcktmpfolder"
-#sudo mkdir "dcktmpfolder"
-#cd "$1/Apps/Node/"
-
-#sudo docker build -t 'python-app' .
-#sudo docker run -P -it --rm --name 'running-01' 'python-app'
- 
-sudo rm "dcktmpfolder" -r
+sudo rm "dcktmpfolder"
 sudo mkdir "dcktmpfolder"
-cd "dcktmpfolder"	
-cd "$1/Apps/Phyton/"
+cd "$1/Apps/Node/"
 
 sudo docker build -t 'node-app' .
-sudo docker run -it --rm --name 'running-02' 'node-app'
+sudo docker run -P -it --rm --name 'running-01' 'node-app'
+ 
+#sudo rm "dcktmpfolder" -r
+#sudo mkdir "dcktmpfolder"
+#cd "dcktmpfolder"	
+#cd "$1/Apps/Phyton/"
+
+#sudo docker build -t 'phyton-app' .
+#sudo docker run -it --rm --name 'running-02' 'phyton-app'
