1728efc40c19a44514e4a7b8833398f62e6cb45b
Enis Sanchez <Enis Sanchez>
2016-04-10 02:43:33 +0100

    m

--- Deploy-Docker-Containers.sh
+++ Deploy-Docker-Containers.sh
@@ -1,13 +1,14 @@
-sudo rm "dcktmpfolder" -r
+sudo rm "dcktmpfolder"
 sudo mkdir "dcktmpfolder"
-cd "dcktmpfolder$1/Apps/Node/"
+cd "dcktmpfolder/$1/Apps/Node/"
 
 sudo docker build -t 'python-app' .
 sudo docker run -P -it --rm --name 'running-01' 'python-app'
  
 sudo rm "dcktmpfolder" -r
-sudo mkdir "dcktmpfolder"	
-cd "dcktmpfolder$1/DockerFiles/Server/"
+sudo mkdir "dcktmpfolder"
+cd "dcktmpfolder"	
+cd "$1/DockerFiles/Server/"
 
 sudo docker build -t 'node-app' .
 sudo docker run -it --rm --name 'running-02' 'node-app'
