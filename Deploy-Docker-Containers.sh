d65b8018e6cd827624b8e73962468fd4f5a2f5ca
Enis Nefzi <enefzi@veinteractive.com>
2016-04-22 01:57:22 +0200

    c

--- Deploy-Docker-Containers.sh
+++ Deploy-Docker-Containers.sh
@@ -3,7 +3,7 @@ sudo mkdir "dcktmpfolder"
 cd "$1/Apps/Node/"
 
 sudo docker build -t 'node-app' .
-sudo docker run -p 127.0.0.1:80:3000 -d -it --name 'running-01' 'node-app'
 
-#sudo docker build -t 'node-app' .
-#sudo docker run -P -d -it --name 'running-02' 'node-app' 
+sudo docker run --expose 3000 -p 8080:3000 -d -it --name 'Node-Server' 'node-app'
+#sudo docker run --expose 4000 -p 127.0.0.1:80:4000 -d -it --name 'Node-Client' 'node-app' --link 'Node-Server'
+
