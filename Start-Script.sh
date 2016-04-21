8d84558ae52eb506e48a392434243d22c97de988
Enis Nefzi <enefzi@veinteractive.com>
2016-04-21 12:32:36 +0200

    commit

--- Start-Script.sh
+++ Start-Script.sh
@@ -15,7 +15,7 @@ apt-get purge lxc-docker*
 apt-get purge docker.io*
 
 #Install CA certificates
-apt-get install apt-transport-https ca-certificates
+apt-get -y install apt-transport-https ca-certificates
 
 #Add the key
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
