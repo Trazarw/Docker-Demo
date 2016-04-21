01d14d6c099e7847c991d493610f2fba1122445f
Enis Nefzi <enefzi@veinteractive.com>
2016-04-22 01:17:00 +0200

    c

--- Start-Script.sh
+++ Start-Script.sh
@@ -10,11 +10,15 @@ apt-get -y install git
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
 rm /etc/apt/sources.list.d/docker.list
-echo 'deb https://apt.dockerproject.org/repo debian-jessie main' > /etc/apt/sources.list.d/docker.list
+echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' > /etc/apt/sources.list.d/docker.list
 
 apt-cache policy docker-engine
 
 apt-get -y install docker-engine
+
+sudo apt-get -y install linux-image-extra-$(uname -r)
+
+apt-get -y install apparmor
 
 service docker start
 
