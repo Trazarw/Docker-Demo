db38f47055932fc5ae2ab0bbc9210564afc61cdf
Enis Nefzi <enefzi@veinteractive.com>
2016-04-22 00:48:55 +0200

    SC

--- Deploy-Docker-Containers.sh
+++ Deploy-Docker-Containers.sh
@@ -6,4 +6,4 @@ sudo docker build -t 'node-app'
 sudo docker run -p 127.0.0.1:80:3000 -d -it --name 'running-01' 'node-app'
 
 #sudo docker build -t 'node-app' .
-#sudo docker run -P -d -it --name 'running-02' 'node-app' 
+#sudo docker run -P -d -it --name 'running-02' 'node-app'  
