fa0cc3367294340303a4c1539ad368fbec1d6f51
Enis Nefzi <enefzi@veinteractive.com>
2016-04-21 11:28:56 +0200

    S

--- Deploy-Docker-Containers.sh
+++ Deploy-Docker-Containers.sh
@@ -4,8 +4,8 @@ cd "$1/Apps/Node/"
 
 (
 	docker build -t 'node-app' .
-	docker run -P -it --rm --name 'running-01' 'node-app'
+	docker run -P -i --rm --name 'running-01' 'node-app'
 	 
 	docker build -t 'node-app' .
-	docker run -P -it --rm --name 'running-02' 'node-app'
+	docker run -P -i --rm --name 'running-02' 'node-app'
 )
