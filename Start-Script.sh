55f55731d8f7113125c4e1e07e95fb1df821c8bd
Enis Nefzi <enefzi@veinteractive.com>
2016-04-21 16:01:26 +0200

    changes

--- Start-Script.sh
+++ Start-Script.sh
@@ -13,7 +13,7 @@ apt-get -y install apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
 rm /etc/apt/sources.list.d/docker.list
-echo 'deb https://apt.dockerproject.org/repo debian-wheezy main' > /etc/apt/sources.list.d/docker.list
+echo 'deb https://apt.dockerproject.org/repo debian-jessie main' > /etc/apt/sources.list.d/docker.list
 
 apt-cache policy docker-engine
 
@@ -31,3 +31,4 @@ scriptPath="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 chmod 555 ./Deploy-Docker-Containers.sh
 ./Deploy-Docker-Containers.sh $scriptPath
+
