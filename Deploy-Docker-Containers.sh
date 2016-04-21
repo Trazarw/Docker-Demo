11436a3aeaa6b1c0b16807380c6d0761bc192384
Enis Nefzi <enefzi@veinteractive.com>
2016-04-21 11:15:41 +0200

    m

--- Deploy-Docker-Containers.sh
+++ Deploy-Docker-Containers.sh
@@ -1,14 +1,9 @@
-sudo rm "dcktmpfolder"
-sudo mkdir "dcktmpfolder"
+rm "dcktmpfolder"
+mkdir "dcktmpfolder"
 cd "$1/Apps/Node/"
 
-sudo docker build -t 'node-app' .
-sudo docker run -P -it --rm --name 'running-01' 'node-app'
+docker build -t 'node-app' .
+docker run -P -it --rm --name 'running-01' 'node-app'
  
-#sudo rm "dcktmpfolder" -r
-#sudo mkdir "dcktmpfolder"
-#cd "dcktmpfolder"	
-#cd "$1/Apps/Phyton/"
-
-#sudo docker build -t 'phyton-app' .
-#sudo docker run -it --rm --name 'running-02' 'phyton-app'
+docker build -t 'node-app' .
+docker run -P -it --rm --name 'running-02' 'node-app'
--- Start-Script.sh
+++ Start-Script.sh
@@ -1,9 +1,14 @@
+#Go to root user
+sudo su
 
 #Update apt get
 apt-get update
 
 #Install curl
-apt-get install curl
+apt-get -y install curl
+
+#Install git
+apt-get -y install git
 
 #Purge old docker
 apt-get purge lxc-docker*
@@ -16,21 +21,21 @@ apt-get install apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
 #Recreate docker file list & add a entry for Deian
-sudo rm /etc/apt/sources.list.d/docker.list
-sudo echo 'deb https://apt.dockerproject.org/repo debian-wheezy main' > /etc/apt/sources.list.d/docker.list
+rm /etc/apt/sources.list.d/docker.list
+echo 'deb https://apt.dockerproject.org/repo debian-wheezy main' > /etc/apt/sources.list.d/docker.list
 
 #Verify that APT is pulling from the right repository.
 apt-cache policy docker-engine
 
 #Install docker
-sudo apt-get install docker-engine
+apt-get -y install docker-engine
 
 #Start docker 
-sudo service docker start
+service docker start
 
 #Cloning the repository
-sudo rm GC-Infra -r
-sudo git clone https://github.com/Trazarw/GC-Infra
+rm GC-Infra -r
+git clone https://github.com/Trazarw/GC-Infra
 
 #Get executing script source
 SOURCE="${BASH_SOURCE[0]}"
@@ -43,6 +48,6 @@ scriptPath="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 #Deploy the docker containers
 cd "$scriptPath/GC-Infra/"
-sudo chmod 555 ./Deploy-Docker-Containers.sh
-sudo ./Deploy-Docker-Containers.sh $scriptPath/GC-Infra
+chmod 555 ./Deploy-Docker-Containers.sh
+./Deploy-Docker-Containers.sh $scriptPath/GC-Infra
 	
