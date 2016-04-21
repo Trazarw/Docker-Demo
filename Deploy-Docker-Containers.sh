e99692965a99a066ee9c7ddf9fd6a24a84b488e3
Enis Nefzi <enefzi@veinteractive.com>
2016-04-21 11:33:37 +0200

    commit

--- Deploy-Docker-Containers.sh
+++ Deploy-Docker-Containers.sh
@@ -2,10 +2,8 @@ rm "dcktmpfolder"
 mkdir "dcktmpfolder"
 cd "$1/Apps/Node/"
 
-(
-	docker build -t 'node-app' .
-	docker run -P -i --rm --name 'running-01' 'node-app'
+docker build -t 'node-app' .
+docker run -P -i --rm --name 'running-01' 'node-app'
 	 
-	docker build -t 'node-app' .
-	docker run -P -i --rm --name 'running-02' 'node-app'
-)
+docker build -t 'node-app' .
+docker run -P -i --rm --name 'running-02' 'node-app'
