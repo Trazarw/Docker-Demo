43d79445830d66bb735f0a970958e1e93449ab31
Enis Nefzi <enefzi@veinteractive.com>
2016-04-22 10:30:28 +0200

    commit

--- Deploy-Docker-Containers.sh
+++ Deploy-Docker-Containers.sh
@@ -1,7 +1,10 @@
 cd "$1/Apps/Node-Proxy"
 sudo docker build -t 'docker-build' .
-sudo docker run --expose 3000 -p 8080:3000 -d -it --name 'Node-Proxy' 'docker-build' -c "npm install sync-request"
+sudo docker run --expose 3000 -p 8080:3000 -d -it --name 'Node-Proxy' 'docker-build' 
+#-c "npm install sync-request"
 
 cd "$1/Apps/Node-Server"
 sudo docker build -t 'docker-build' .
-sudo docker run -d -it --name 'Node-Server' 'docker-build' --link 'Node-Proxy':'Proxy'
+sudo docker run -d -it --name 'Node-Server' 'docker-build' 
+
+#--link 'Node-Proxy':'Proxy'
