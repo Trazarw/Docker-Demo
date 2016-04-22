608935d04773a76be4b5482822e6805f1579cbab
Enis Nefzi <enefzi@veinteractive.com>
2016-04-22 11:09:43 +0200

    commit

--- Apps/Node-Proxy/app.js
+++ Apps/Node-Proxy/app.js
@@ -1,12 +1,10 @@
 var http = require('http');
-var request = require('sync-request');
+//var request = require('sync-request');
 
 http.createServer(function (req, res) {
-  var res = request('GET', '0.0.0.0:8081');
+  //var res = request('GET', '0.0.0.0:8081');
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Proxied Message:\n' + res.getBody());
 }).listen(4000, '0.0.0.0');
 
 console.log('Server running at http://0.0.0.0:4000/');
-
-	
