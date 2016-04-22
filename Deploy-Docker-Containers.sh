ad2406500ad39276c4ad0a9aa212ef7aedbb77c3
Enis Nefzi <enefzi@veinteractive.com>
2016-04-22 03:41:50 +0200

    c

--- Deploy-Docker-Containers.sh
+++ Deploy-Docker-Containers.sh
@@ -2,6 +2,6 @@ cd "$1/Apps/Node-Proxy"
 sudo docker build -t 'Docker-Build' .
 sudo docker run --expose 3000 -p 8080:3000 -d -it --name 'Node-Proxy' 'Docker-Build'
 
-cd "$1/Apps/Node-Server"
-sudo docker build -t 'Docker-Build' .
-sudo docker run -d -it --name 'Node-Server' 'Docker-Build' --link 'Node-Proxy'
+#cd "$1/Apps/Node-Server"
+#sudo docker build -t 'Docker-Build' .
+#sudo docker run -d -it --name 'Node-Server' 'Docker-Build' --link 'Node-Proxy'
