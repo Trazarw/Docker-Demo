d3e2d38a73d6f6bb71f5e56cbfe73095a3183773
Enis Nefzi <enefzi@veinteractive.com>
2016-04-22 11:58:54 +0200

    commit

--- Apps/Node-Proxy/app.js
+++ Apps/Node-Proxy/app.js
@@ -1,6 +1,18 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
+
+	var req = http.request({
+	    host: '192.168.5.8',
+	    port: 3128,
+	    method: 'GET',
+	    path: '/'
+	}, function (res) {
+	    res.on('data', function (data) {
+	        console.log(data.toString());
+	    });
+	});
+  req.end();
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Ve Bilbao Rocks\n');
 }).listen(4000, '0.0.0.0');
