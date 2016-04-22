2d2a8f14c1b069838a483a31c9bf4da875838797
Enis Nefzi <enefzi@veinteractive.com>
2016-04-22 03:16:30 +0200

    c

--- Deploy-Docker-Containers.sh
+++ Deploy-Docker-Containers.sh
@@ -4,7 +4,7 @@ cd "$1/Apps/Node/"
 
 sudo docker build -t 'node-app' .
 
-sudo docker run --expose 3000 -p 8080:3000 -d -it --name 'Node-Server' 'node-app' --network.host _non_loopback_
+sudo docker run --expose 3000 -p 8080:3000 -d -it --name 'Node-Server' 'node-app'
 #sudo docker run --expose 4000 -p 127.0.0.1:80:4000 -d -it --name 'Node-Client' 'node-app' --link 'Node-Server'
 
 	
