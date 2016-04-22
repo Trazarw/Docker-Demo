62194395198d432f619543025e59c614bb9301ee
Enis Nefzi <enefzi@veinteractive.com>
2016-04-22 11:06:03 +0200

    commit

--- Apps/Node-Proxy/app.js
+++ Apps/Node-Proxy/app.js
@@ -2,7 +2,7 @@ var http = require('http');
 var request = require('sync-request');
 
 http.createServer(function (req, res) {
-  var res = request('GET', 'http://0.0.0.0:8081');
+  var res = request('GET', '0.0.0.0:8081');
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Proxied Message:\n' + res.getBody());
 }).listen(4000, '0.0.0.0');
--- Deploy-Docker-Containers.sh
+++ Deploy-Docker-Containers.sh
@@ -1,10 +1,10 @@
 cd "$1/Apps/Node-Proxy"
-sudo docker build -t 'docker-build' .
-sudo docker run --expose 3000 -p 8080:4000 -d -it --name 'Node-Proxy' 'docker-build' 
+sudo docker build -t 'proxy-docker-build' .
+sudo docker run --expose 4000 -p 8080:4000 -d -it --name 'Node-Proxy' 'proxy-docker-build' 
 #-c "npm install sync-request"
 
 cd "$1/Apps/Node-Server"
-sudo docker build -t 'docker-build' .
-sudo docker run --expose 3000 -p 8081:3000 -d -it --name 'Node-Server' 'docker-build'
+sudo docker build -t 'server-docker-build' .
+sudo docker run --expose 3000 -p 8081:3000 -d -it --name 'Node-Server' 'server-docker-build'
 
 #--link 'Node-Proxy':'Proxy'
