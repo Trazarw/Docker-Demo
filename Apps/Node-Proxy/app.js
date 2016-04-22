c4004df3ecf407d3d43334689d155c0c2cfb1109
Enis Nefzi <enefzi@veinteractive.com>
2016-04-22 10:54:52 +0200

    commit

--- Apps/Node-Proxy/app.js
+++ Apps/Node-Proxy/app.js
@@ -5,6 +5,8 @@ http.createServer(function (req, res) {
   var res = request('GET', 'http://0.0.0.0:8081');
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Proxied Message:\n' + res.getBody());
-}).listen(3000, '0.0.0.0');
+}).listen(4000, '0.0.0.0');
+
+console.log('Server running at http://0.0.0.0:4000/');
 
 	
--- Apps/Node-Server/app.js
+++ Apps/Node-Server/app.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Ve Bilbao Rocks\n');
 }).listen(3000, '0.0.0.0');
 
 console.log('Server running at http://0.0.0.0:3000/');
